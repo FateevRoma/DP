787639320de541e861eb2a40a2f7d6a340eeed81
User <fateevroma59@gmail.com>
2025-06-21 18:49:11 +0300

    Add requirements.txt

--- init_db.py
+++ init_db.py
@@ -1,3 +1,20 @@
-from app_cleaned import db
-db.create_all()
-print("Базу даних створено успішно.")
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    subcategory = db.Column(db.String(100), nullable=True)
+    image = db.Column(db.String(200), nullable=True)
+
+with app.app_context():
+    db.create_all()
+    print("✅ Базу даних створено з полем image.")
